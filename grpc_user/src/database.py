494ee0f71dd99a2aa802bfe07edeb702cdfadf70
Yury Zimarev <yury.zimarev@gmail.com>
2024-04-12 22:11:04 +0300

    pgsql manager for user service

---
+++ grpc_user/src/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+import os
+import logging
+from src.models import Base
+
+class DatabaseManager:
+    def __init__(self):
+        username = os.getenv("POSTGRES_USERNAME")
+        password = os.getenv("POSTGRES_PASSWORD")
+        url = os.getenv("POSTGRES_URL")
+        db_name = os.getenv("POSTGRES_DB_NAME")
+        DSN_URL = f"postgresql+asyncpg://{username}:{password}@{url}/{db_name}"
+        self.url = DSN_URL
+
+    async def connect(self):
+        self.engine = create_async_engine(
+            url = self.url,
+            echo=True,
+            pool_size=5,
+            max_overflow=10
+        )
+        logging.info("Succesfully connected to PGSQL Database!")
+
+    async def shutdown(self):
+        await self.engine.dispose()
+        logging.info("Connection to PGSQL Database was closed")
+
+    async def initialize_schema(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+        logging.info("Successfully innitialized schema!")
