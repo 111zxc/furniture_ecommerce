ec28151b4ab77568622fc720abc5668707514718
Yury Zimarev <yury.zimarev@gmail.com>
2024-04-16 16:03:03 +0300

    Brushed auth service

---
+++ grpc_auth/src/redis_service.py
@@ -0,0 +1,33 @@
+import logging
+import os
+from typing import Any, Literal
+
+from redis import asyncio as aioredis
+
+
+class RedisManager:
+    def __init__(self) -> None:
+        self.redis_pool = None
+
+    async def connect(self) -> None:
+        try:
+            host = os.getenv("HOSTNAME")
+            self.redis_pool = aioredis.ConnectionPool.from_url(f"redis://{host}")
+            logging.info("Connected to Redis")
+        except Exception as e:
+            logging.error(f"Error connecting to Redis: {e}")
+            raise
+
+    async def close(self) -> None:
+        if self.redis_pool:
+            await self.redis_pool.aclose()
+            logging.info("Disconnected from Redis")
+
+    async def add_revoked_token(self, token) -> None:
+        async with aioredis.Redis.from_pool(self.redis_pool) as conn:
+            await conn.sadd("revoked_tokens", token)
+
+    async def is_token_revoked(self, token) -> Any | Literal[0, 1]:
+        async with aioredis.Redis.from_pool(self.redis_pool) as conn:
+            result = await conn.sismember("revoked_tokens", token)
+        return result
